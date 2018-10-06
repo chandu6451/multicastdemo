e38b2e688622f989bc4e6e02c43cff025f87aa93
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 14:55:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegate
+{
+    delegate void CalculateDelegate(int val1, int val2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate c = new Calculate();
+
+            CalculateDelegate calDel = new CalculateDelegate(c.Add);
+            calDel += new CalculateDelegate(c.Divide);
+            calDel += new CalculateDelegate(Calculate.Subtract);
+            calDel += c.Add;
+            calDel += Calculate.Multiply;
+
+            calDel(35, 5);
+
+            Delegate[] invList = calDel.GetInvocationList();
+            Console.WriteLine("\nInvocation List : ");
+            for (int i = 0; i < invList.Length; i++)
+            {
+                Console.WriteLine(invList[i].Method);
+            }
+
+            calDel -= c.Add;
+            calDel(30, 3);
+
+            Console.ReadKey();
+        }
+    }
+}
